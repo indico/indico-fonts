fbac94378c5af1adbae304bc732cf31cb6d70e0c
Tomas R <tomas.roun8@gmail.com>
2023-07-11 20:02:25 +0200

    Add CJK fonts + clean-up (#3)
    * Add the Google Noto fonts (serif, sans-serif, mono)
    
    The Google Noto is a versatile font which allows Indico
    to render Japanese, Chinese and Korean characters.
    
    * Remove legacy license header
    * Update package for Python 3
    * Bump version
    * Switch to setup.cfg
    * Use Unix line-endings in license files
    * Improve license distribution
    
    Include the licenses as package data so that they are kept together
    with the installed files and remove unneeded package-data
    specification.
    
    Co-authored-by: Björn Pedersen <bjoern.pedersen@frm2.tum.de>

--- setup.py
+++ setup.py
@@ -1,36 +1,3 @@
-# This file is part of Indico.
-# Copyright (C) 2002 - 2015 European Organization for Nuclear Research (CERN).
-#
-# Indico is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 3 of the
-# License, or (at your option) any later version.
-#
-# Indico is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Indico; if not, see <http://www.gnu.org/licenses/>.
-
-from __future__ import unicode_literals
-
 from setuptools import setup
 
-setup(
-    name="indico-fonts",
-    version='1.1',
-    url='https://getindico.io',
-    license='Several',
-    author='Indico Team',
-    author_email='indico-team@cern.ch',
-    description='Indico - Binary fonts',
-    platforms=['any'],
-    zip_safe=False,
-    packages=[b'indico_fonts'],
-    package_data={b'indico_fonts': [b'*.otf', b'*.ttc', b'*.ttf']},
-    include_package_data=True,
-    classifiers=['License :: Other/Proprietary License',
-                 'Topic :: Text Processing :: Fonts']
-)
+setup()
