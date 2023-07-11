82c81a0de6801f7124bfdb28211c8a382972e770
Tomas Roun <tomas.roun@cern.ch>
2023-07-11 16:42:09 +0200

    Switch to setup.cfg

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='indico-fonts',
-    version='1.2',
-    url='https://getindico.io',
-    license='Several',
-    author='Indico Team',
-    author_email='indico-team@cern.ch',
-    description='Indico - Binary fonts',
-    platforms=['any'],
-    zip_safe=False,
-    packages=['indico_fonts'],
-    package_data={'indico_fonts': ['*.otf', '*.ttc', '*.ttf']},
-    include_package_data=True,
-    classifiers=['License :: Other/Proprietary License',
-                 'Topic :: Text Processing :: Fonts']
-)
+setup()
